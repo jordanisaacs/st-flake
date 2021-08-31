9eb7b2d7e3fd2aea1ed762e23cfc9d9eed683a4a
Jordan Isaacs <github@jdisaacs.com>
2021-08-30 20:27:08 -0700

    base st

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              stJD = "https://";
+            })
+          ];
+        };
+      in
+      rec {
+        apps = {
+          st = {
+            type = "app";
+            program = "${defaultPackage}/bin/st";
+          };
+        };
+
+        packages.stJD = pkgs.stJD;
+        defaultApp = apps.st;
+        defaultPackage = pkgs.stJD;
+
+
+      }
+    );
+}
